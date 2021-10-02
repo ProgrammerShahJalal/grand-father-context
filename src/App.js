7e00c2f97e73026400824d348a1e3e5355a8ef89
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-10-02 20:46:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import GrandFather from './components/GrandFather/GrandFather';
+import { createContext, useState } from 'react';
+
+export const RingContext = createContext('ring');
+
+function App() {
+  const [house, setHouse] = useState(2);
+  const ornaments = 'Shokto Mati Ring'
+  return (
+    <RingContext.Provider value={[ornaments, house]}>
+      <div className="App">
+        <button
+          onClick={() => setHouse(house + 1)}
+        >Buy a new house</button>
+
+        <GrandFather house={house}></GrandFather>
+      </div>
+    </RingContext.Provider>
+  );
+}
+
+export default App;
